e988a42666daceabb0b058954c2bbdac34046f5d
birolezgin <b.ezginksf@gmail.com>
2020-08-23 21:46:19 -0700

    app.js application codes developed

--- app.js
+++ app.js
@@ -10,6 +10,177 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// array of employees
+const employees = []
+
+// questions for if manager role is selected
+function questionsToAskManager(type){
+    inquirer.prompt([
+        {
+            message: 'What is your name?',
+            name: 'name',
+            type: 'input',
+        },
+        {
+            message: 'Enter your ID',
+            name: 'id',
+            type: 'input',
+        },
+        {
+            message: 'What is your email?',
+            name: 'email',
+            type: 'input',
+        },
+        {
+            message: 'What is your office number?',
+            name: 'office',
+            type: 'input',
+        },
+    ]
+    ).then(response=>{
+        switch(type){
+            case "Manager":
+                let manager = new Manager(response.name, response.id, response.email, response.office, type)
+                employees.push(manager)
+                newRole()
+            break;
+        }
+    })
+}
+
+// questions for intern role
+function questionsToAskIntern(type){
+    inquirer.prompt([
+        {
+            message: 'What is your name?',
+            name: 'name',
+            type: 'input',
+        },
+        {
+            message: 'Enter your ID?',
+            name: 'id',
+            type: 'input',
+        },
+        {
+            message: 'What is your email?',
+            name: 'email',
+            type: 'input',
+        },
+        {
+            message: 'Where do you study?',
+            name: 'school',
+            type: 'input',
+        },
+    ]
+    ).then(response=>{
+        switch(type){
+            case "Intern":
+                let intern = new Intern(response.name, response.id, response.email, response.school, type)
+                employees.push(intern)
+                newRole()
+            break;
+        }
+    })
+}
+
+// questions for Engineer role
+function questionsToAskEngineer(type){
+    inquirer.prompt([
+
+        {
+            message: 'What is your name?',
+            name: 'name',
+            type: 'input'
+        },
+        {
+            message: 'Enter your id?',
+            name: 'id',
+            type: 'input',
+        },
+        {
+            message: 'What is you email?',
+            name: 'email',
+            type: 'input',
+        },
+        {
+            message: 'What is your GitHub?',
+            name: 'github',
+            type: 'input',
+        },
+    ]
+    ).then(response=>{
+        switch(type){
+            case "Engineer":
+                let engineer = new Engineer(response.name, response.id, response.email, response.github, type)
+                employees.push(engineer)
+                newRole()
+            break;
+        }
+    })
+}
+
+// to select a role and populate the correct questions for said role
+function promptRole() {
+    return inquirer.prompt({
+        message: 'Select a role',
+        name: 'role',
+        type: 'list',
+        choices: ['Manager', 'Engineer', 'Intern']
+    })
+    .then(answer => {
+        console.log(answers.role)
+        //if manager is choosen
+        if (answer.role == 'manager') {
+            questionsToAskManager(answer.role);
+        }else if (answer.role == 'Engineer') {
+            //if engineer is choosen
+            questionsToAskEngineer(answer.role);
+        }else {
+            //if intern is choosen
+            questionsToAskIntern(answer.role);
+        }
+    })
+    .catch(error => {
+        if(err) {
+            console.log(error)
+        }
+    });
+}
+
+// create role selection to start or end the process
+function newRole() {
+    return inquirer.prompt({
+        message:'Add member to the team?',
+        name: 'add',
+        type: 'list',
+        choices: ['Yes', 'No']
+    }).then(answers => {
+        if (answers.add == 'Yes') {
+            promptRole()
+        }else if (answers.add == 'No') {
+
+            fs.writeFile(outputPath, render(employees), function (err) {
+                if (err) {
+                    throw err
+                }
+            })
+            console.log('all done!')
+        }
+    }).catch(error => {
+        if (err) {
+            console.log(error)
+      
+        }
+    })
+}
+
+//to show employees are being pushed to the array
+function showEmployees(){
+    console.log(employees)
+}
+
+promptRole()
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
